70b3d10af297078cf848168f5b7668a5e53da08f
Phiso Hu <33883485+YiXiaoCuoHuaiFenZi@users.noreply.github.com>
2024-01-04 14:35:30 +0800

    3

--- tests/proto/main.c
+++ tests/proto/main.c
@@ -15,8 +15,11 @@ int main(int argc, char* args[])
 		return 1;
 //
 //	// test for format proto files under a directory.
-//	if (strcmp(args[1], "test_format_directory") == 0)
-//		return test_format_directory("test_data/malformed", "test_data/formatted");
+	if (strcmp(args[1], "test_format_directory") == 0){
+		printf("(strcmp(args[1], \"test_format_directory\") == 0).\n");
+		return test_format_directory("test_data/malformed", "test_data/formatted");
+	}
+
 	printf("test proto aaa.\n");
 	return 0;
 }
