34059c7e4b1ed0079b2cd35c12d047309f8d4461
Phiso Hu <33883485+YiXiaoCuoHuaiFenZi@users.noreply.github.com>
2024-01-04 14:30:48 +0800

    Update main.c

--- tests/proto/main.c
+++ tests/proto/main.c
@@ -11,12 +11,12 @@ int main(int argc, char* args[])
 {
 	printf("test proto.\n");
 	printf("test proto %d.\n", argc);
-	if (argc < 2)
-		return 1;
-
-	// test for format proto files under a directory.
-	if (strcmp(args[1], "test_format_directory") == 0)
-		return test_format_directory("test_data/malformed", "test_data/formatted");
+//	if (argc < 2)
+//		return 1;
+//
+//	// test for format proto files under a directory.
+//	if (strcmp(args[1], "test_format_directory") == 0)
+//		return test_format_directory("test_data/malformed", "test_data/formatted");
 
 	return 0;
 }
